bbbf4a31e339358f3d51ed12313d3260b755ce51
Vladislav Eryomenko <80623364+thnxvlad@users.noreply.github.com>
2022-03-08 00:43:33 +0300

    deployment to large dimensions

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
 
-var epsilon float64 = 1.0 / 10000
+const EPSILON float64 = 1.0 / 10000
+const MAX_ITERATIONS int = 100
 
+// vec * num
 func calcProductVectorByNum(vec []float64, num float64) []float64 {
 	resultVec := make([]float64, len(vec))
 	for i, el := range vec {
@@ -15,16 +18,30 @@ func calcProductVectorByNum(vec []float64, num float64) []float64 {
 	return resultVec
 }
 
-func subtractVector(vec1 []float64, vec2 []float64) {
-	for i, _ := range vec1 {
-		vec1[i] -= vec2[i]
-		if math.Abs(vec1[i]) < epsilon {
-			vec1[i] = 0
-		}
+// vec1 + vec2
+func calcVectorSum(vec1 []float64, vec2 []float64) []float64 {
+	resultVec := make([]float64, len(vec1))
+	for i, _ := range resultVec {
+		resultVec[i] = vec1[i] + vec2[i]
 	}
+	return resultVec
 }
 
-func gauss(_A [][]float64, _b []float64) []float64 {
+// vec1 - vec2
+func calcVectorDifference(vec1 []float64, vec2 []float64) []float64 {
+	return calcVectorSum(vec1, calcProductVectorByNum(vec2, -1))
+}
+
+func calcVectorRounding(vec []float64) []float64 {
+	resultVec := make([]float64, len(vec))
+	for i, el := range vec {
+		resultVec[i] = math.Round(el/EPSILON) * EPSILON
+	}
+	return resultVec
+}
+
+// Ax = b
+func gaussMethod(_A [][]float64, _b []float64) []float64 {
 	A := make([][]float64, len(_A))
 	for i, slice := range _A {
 		A[i] = make([]float64, len(slice))
@@ -35,21 +52,18 @@ func gauss(_A [][]float64, _b []float64) []float64 {
 	for i := 0; i < len(A); i++ {
 		var pivotElementAbs float64 = math.Abs(A[i][i])
 		var pivotElementIdx int = i
-		for idx, vec := range A[i+1:] {
+		for j, vec := range A[i+1:] {
 			if math.Abs(vec[i]) > pivotElementAbs {
 				pivotElementAbs = math.Abs(vec[i])
-				pivotElementIdx = idx + i + 1
+				pivotElementIdx = j + i + 1
 			}
 		}
 		A[i], A[pivotElementIdx] = A[pivotElementIdx], A[i]
 		b[i], b[pivotElementIdx] = b[pivotElementIdx], b[i]
 		for j := i + 1; j < len(A); j++ {
 			var scalingMultiplier float64 = A[j][i] / A[i][i]
-			subtractVector(A[j], calcProductVectorByNum(A[i], scalingMultiplier))
+			A[j] = calcVectorDifference(A[j], calcProductVectorByNum(A[i], scalingMultiplier))
 			b[j] -= scalingMultiplier * b[i]
-			if math.Abs(b[j]) < epsilon {
-				b[j] = 0
-			}
 		}
 	}
 
@@ -57,35 +71,52 @@ func gauss(_A [][]float64, _b []float64) []float64 {
 		for j := i + 1; j < len(A); j++ {
 			b[i] -= b[j] * A[i][j]
 		}
-		b[i] = math.Round(b[i]/A[i][i]/epsilon) * epsilon
+		b[i] /= A[i][i]
 	}
+	b = calcVectorRounding(b)
 	return b
 }
 
-func calcFnVec(fnVec []func(float64, float64) float64, x float64, y float64) []float64 {
-	resVec := make([]float64, len(fnVec))
+func calcFnVec(fnVec []func([]float64) float64, X []float64) []float64 {
+	resultVec := make([]float64, len(fnVec))
 	for i, fn := range fnVec {
-		resVec[i] = fn(x, y)
+		resultVec[i] = fn(X)
 	}
-	return resVec
+	return resultVec
 }
 
-func calcJacobiMatrix(W [][]func(float64, float64) float64, x float64, y float64) [][]float64 {
-	resMatrix := make([][]float64, len(W))
+func calcJacobiMatrix(W [][]func([]float64) float64, X []float64) [][]float64 {
+	resultMatrix := make([][]float64, len(W))
 	for i, fnVec := range W {
-		resMatrix[i] = calcFnVec(fnVec, x, y)
+		resultMatrix[i] = calcFnVec(fnVec, X)
 	}
-	return resMatrix
+	return resultMatrix
 }
 
-func newtonMethod(F []func(float64, float64) float64, W [][]func(float64, float64) float64, x float64, y float64, iterationsNum int) {
-	data := gauss(calcJacobiMatrix(W, x, y), calcProductVectorByNum(calcFnVec(F, x, y), -1))
-	newX := x + data[0]
-	newY := y + data[1]
-	if math.Max(math.Abs(data[0]), math.Abs(data[1])) > epsilon {
-		newtonMethod(F, W, newX, newY, iterationsNum+1)
-	} else {
-		fmt.Println("x: ", newX, ", y: ", newY)
+func newtonMethod(F []func([]float64) float64, W [][]func([]float64) float64, X []float64) ([]float64, int, error) {
+	newX := X
+	var iteration int = 0
+	for {
+		if iteration == MAX_ITERATIONS {
+			return calcVectorRounding(newX), iteration, errors.New("iteration limit exceeded")
+		} else {
+			iteration++
+		}
+		dX := gaussMethod(calcJacobiMatrix(W, newX), calcProductVectorByNum(calcFnVec(F, newX), -1))
+		newX = calcVectorSum(newX, dX)
+		var calcContinueCondition bool = false
+		for _, dx := range dX {
+			if dx > EPSILON {
+				calcContinueCondition = true
+				break
+			}
+		}
+		switch calcContinueCondition {
+		case true:
+			continue
+		case false:
+			return calcVectorRounding(newX), iteration, nil
+		}
 	}
 }
 
@@ -99,32 +130,36 @@ func main() {
 	b := []float64{
 		-14, 44, 142, -76,
 	}
-	fmt.Println(gauss(A, b))
-	F := []func(float64, float64) float64{
-		func(x float64, y float64) float64 {
-			return math.Sin(2*x-y) - 1.2*x - 0.4
+	fmt.Println(gaussMethod(A, b))
+	F := []func([]float64) float64{
+		func(X []float64) float64 {
+			return math.Sin(2*X[0]-X[1]) - 1.2*X[0] - 0.4
 		},
-		func(x float64, y float64) float64 {
-			return 0.8*math.Pow(x, 2) + 1.5*math.Pow(y, 2) - 1
-		},
-	}
-	W := [][]func(float64, float64) float64{
-		{
-			func(x float64, y float64) float64 {
-				return 2*math.Cos(2*x-y) - 1.2
-			},
-			func(x float64, y float64) float64 {
-				return -math.Cos(2*x - y)
-			},
-		},
-		{
-			func(x float64, _ float64) float64 {
-				return 1.6 * x
-			},
-			func(_ float64, y float64) float64 {
-				return 3 * y
-			},
+		func(X []float64) float64 {
+			return 0.8*math.Pow(X[0], 2) + 1.5*math.Pow(X[1], 2) - 1
 		},
 	}
-	newtonMethod(F, W, 0.4, -0.75, 1)
+	W := [][]func([]float64) float64{
+		{
+			func(X []float64) float64 {
+				return 2*math.Cos(2*X[0]-X[1]) - 1.2
+			},
+			func(X []float64) float64 {
+				return -math.Cos(2*X[0] - X[1])
+			},
+		},
+		{
+			func(X []float64) float64 {
+				return 1.6 * X[0]
+			},
+			func(X []float64) float64 {
+				return 3 * X[1]
+			},
+		},
+	}
+	X := []float64{
+		0.4,
+		-0.75,
+	}
+	fmt.Println(newtonMethod(F, W, X))
 }
